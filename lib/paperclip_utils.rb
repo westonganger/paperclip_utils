9ff9c2931c18230a5f8ccad9e85a791b829afe96
Weston Ganger <westonganger@gmail.com>
2016-04-12 14:39:26 -0700

    release v1.0.2

--- lib/paperclip_utils.rb
+++ lib/paperclip_utils.rb
@@ -17,10 +17,17 @@ module Paperclip
       return (allowed_content_types.include?(content_type) ? processors : fallback_processors)
     end
 
-    def self.get_styles(content_type, to_styles={preview: "800x600>", thumb: "100x100>"}, fallback_styles={}, allowed_content_types=ALLOWED_CONTENT_TYPES)
+    def self.get_styles(content_type, to_styles=, fallback_styles={}, allowed_content_types=ALLOWED_CONTENT_TYPES)
+      unless to_styles
+        if ['application/pdf','image/tiff','image/tif','image/x-tiff'].include?(content_type)
+          to_styles = {preview: ["800x600>", :png], thumb: ["100x100>", :png]}
+        else
+          to_styles = {preview: "800x600>", thumb: "100x100>"}
+        end
+      end
       return (allowed_content_types.include?(content_type) ? to_styles : fallback_styles)
     end
 
-    ALLOWED_CONTENT_TYPES = ['application/pdf', 'image/png', 'image/x-png', 'image/gif', 'image/jpeg', 'image/pjpeg', 'image/jpg', 'image/tif, ''image/tiff', 'image/x-tiff']
+    ALLOWED_CONTENT_TYPES = ['application/pdf', 'image/png', 'image/x-png', 'image/gif', 'image/jpeg', 'image/pjpeg', 'image/jpg', 'image/tif', 'image/tiff', 'image/x-tiff']
   end
 end
