b242fc812b865356687ee55a52680cc09ae8f38a
Weston Ganger <westonganger@gmail.com>
2016-04-12 14:08:50 -0700

    release v1.0.1

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 # Paperclip Utils
 <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=VKY8YAWAS5XRQ&lc=CA&item_name=Weston%20Ganger&item_number=paperclip_utils&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donate_SM%2egif%3aNonHostedGuest" target="_blank" title="Donate"><img src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" alt="Donate"/></a>
 
-Paperclip Utils adds a few helper methods to ActiveRecord::Base for easier dynamic processors and styles on your Paperclip file uploads
+Paperclip Utils is a helper class for easier dynamic processors and styles on your Paperclip file uploads
 
 
 # Install
 ```ruby
 gem install paperclip_utils
-
-
 ```
-Paperclip Utils contains the following methods to ActiveRecord::Base:
+
+
+Paperclip Utils contains the following methods:
 ```ruby
 Paperclip::Utils.get_processors(content_type, processors_if_allowed, fallback_processors, allowed_content_types)
 
--- lib/paperclip_utils.rb
+++ lib/paperclip_utils.rb
@@ -3,7 +3,7 @@ require 'paperclip_processors/ghostscript.rb'
 
 module Paperclip
   class Utils
-    def self.get_paperclip_processors(content_type, to_processors=nil, fallback_processors=[], allowed_content_types=PAPERCLIP_UTILS_ALLOWED_CONTENT_TYPES)
+    def self.get_processors(content_type, to_processors=nil, fallback_processors=[], allowed_content_types=ALLOWED_CONTENT_TYPES)
       if to_processors && to_processors.is_a?(Array)
         processors = to_processors
       end
@@ -17,10 +17,10 @@ module Paperclip
       return (allowed_content_types.include?(content_type) ? processors : fallback_processors)
     end
 
-    def self.get_paperclip_styles(content_type, to_styles={preview: "800x600>", thumb: "100x100>"}, fallback_styles={}, allowed_content_types=PAPERCLIP_UTILS_ALLOWED_CONTENT_TYPES)
+    def self.get_styles(content_type, to_styles={preview: "800x600>", thumb: "100x100>"}, fallback_styles={}, allowed_content_types=ALLOWED_CONTENT_TYPES)
       return (allowed_content_types.include?(content_type) ? to_styles : fallback_styles)
     end
 
-    PAPERCLIP_UTILS_ALLOWED_CONTENT_TYPES = ['application/pdf', 'image/png', 'image/x-png', 'image/gif', 'image/jpeg', 'image/pjpeg', 'image/jpg', 'image/tif, ''image/tiff', 'image/x-tiff']
+    ALLOWED_CONTENT_TYPES = ['application/pdf', 'image/png', 'image/x-png', 'image/gif', 'image/jpeg', 'image/pjpeg', 'image/jpg', 'image/tif, ''image/tiff', 'image/x-tiff']
   end
 end
