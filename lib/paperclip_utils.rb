173e77c14569ec7f292616186aaf617b46988c24
Weston Ganger <westonganger@gmail.com>
2016-04-13 09:23:25 -0700

    release v1.2.2

--- lib/paperclip_utils.rb
+++ lib/paperclip_utils.rb
@@ -3,16 +3,14 @@ require 'paperclip_processors/ghostscript.rb'
 
 module Paperclip
   class Utils
-    def self.get_processors(content_type, to_processors: [:thumbnail], fallback_processors: [], allowed_content_types: ALLOWED_CONTENT_TYPES)
-      if to_processors
-        if to_processors.is_a?(Array)
-          processors = to_processors
-
+    def self.get_processors(content_type, processors: [:thumbnail], fallback_processors: [], allowed_content_types: ALLOWED_CONTENT_TYPES)
+      if processors
+        if processors.is_a?(Array)
           if processors.include?(:thumbnail) && content_type == 'application/pdf' && !processors.include?(:ghostscript)
             processors.unshift(:ghostscript)
           end
-        elsif to_processors.is_a?(Symbol)
-          processors = [to_processors]
+        elsif processors.is_a?(Symbol)
+          processors = [processors]
         else
           processors = []
         end
@@ -31,17 +29,17 @@ module Paperclip
       return (allowed_content_types.include?(content_type) ? processors : fallback_processors)
     end
 
-    def self.get_styles(content_type, to_styles: {preview: "800x600>", thumb: "100x100>"}, fallback_styles: {}, allowed_content_types: ALLOWED_CONTENT_TYPES)
-      if to_styles
+    def self.get_styles(content_type, styles: {preview: "800x600>", thumb: "100x100>"}, fallback_styles: {}, allowed_content_types: ALLOWED_CONTENT_TYPES)
+      if styles
         if ['application/pdf','image/tiff','image/tif','image/x-tiff'].include?(content_type)
-          to_styles.each do |k,v|
-            to_styles[k] = [(v.is_a?(String) ? v : v[0]), :jpg]
+          styles.each do |k,v|
+            styles[k] = [(v.is_a?(String) ? v : v[0]), :jpg]
           end
         end
       else
-        to_styles = []
+        styles = []
       end
-      return (allowed_content_types.include?(content_type) ? to_styles : fallback_styles)
+      return (allowed_content_types.include?(content_type) ? styles : fallback_styles)
     end
 
     ALLOWED_CONTENT_TYPES = ['application/pdf', 'image/png', 'image/x-png', 'image/gif', 'image/jpeg', 'image/pjpeg', 'image/jpg', 'image/tif', 'image/tiff', 'image/x-tiff']
--- lib/paperclip_utils/version.rb
+++ lib/paperclip_utils/version.rb
@@ -1,3 +1,3 @@
 module PaperclipUtils
-  VERSION = '1.2.1'
+  VERSION = '1.2.2'
 end
