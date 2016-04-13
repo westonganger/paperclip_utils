f8c266c687e42400d21038c55da293f2d0993cd2
Weston Ganger <westonganger@gmail.com>
2016-04-13 08:55:44 -0700

    release v1.2.0

--- lib/paperclip_utils.rb
+++ lib/paperclip_utils.rb
@@ -3,15 +3,29 @@ require 'paperclip_processors/ghostscript.rb'
 
 module Paperclip
   class Utils
-    def self.get_processors(content_type, to_processors=nil, fallback_processors=[], allowed_content_types=ALLOWED_CONTENT_TYPES)
-      if to_processors && to_processors.is_a?(Array)
-        processors = to_processors
-      end
+    def self.get_processors(content_type, options={to_processors: [:thumbnail], fallback_processors: [], allowed_content_types: ALLOWED_CONTENT_TYPES})
+      if options[:to_processors]
+        if options[:to_processors].is_a?(Array)
+          processors = options[:to_processors]
 
-      if content_type == 'application/pdf'
-        processors ||= [:ghostscript, :thumbnail]
-      else
-        processors ||= [:thumbnail]
+          if processors.include?(:thumbnail) && content_type == 'application/pdf' && !processors.include?(:ghostscript)
+            processors.unshift(:ghostscript)
+          end
+        elsif options[:to_processors].is_a?(Symbol)
+          processors = [options[:to_processors]]
+        else
+          processors = []
+        end
+      end
+      
+      if options[:fallback_processors] != []
+        if options[:fallback_processors].is_a?(Array)
+          fallback_processors = options[:fallback_processors]
+        elsif options[:fallback_processors].is_a?(Symbol)
+          fallback_processors = [options[:fallback_processors]]
+        else
+          fallback_processors = []
+        end
       end
       
       return (allowed_content_types.include?(content_type) ? processors : fallback_processors)
