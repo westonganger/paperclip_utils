85b28c3b102488581882bec5a158ee30f0e95426
Weston Ganger <westonganger@gmail.com>
2016-04-13 08:38:44 -0700

    release v1.1.2wq

--- lib/paperclip_utils.rb
+++ lib/paperclip_utils.rb
@@ -17,11 +17,13 @@ module Paperclip
       return (allowed_content_types.include?(content_type) ? processors : fallback_processors)
     end
 
-    def self.get_styles(content_type, to_styles=nil, fallback_styles={}, allowed_content_types=ALLOWED_CONTENT_TYPES)
+    def self.get_styles(content_type, options={to_styles: nil, fallback_styles: {}, allowed_content_types: ALLOWED_CONTENT_TYPES})
+      to_styles = options[:to_styles]
+
       if to_styles
-        if ['application/pdf','image/tiff','image/tif','image/x-tiff'].include?(content_type) && to_styles.any?{|k,v| v.is_a?(String)}
+        if ['application/pdf','image/tiff','image/tif','image/x-tiff'].include?(content_type)
           to_styles.each do |k,v|
-            to_styles[k] = [v, :jpg]
+            to_styles[k] = [(v.is_a?(String) ? v : v[0]), :jpg]
           end
         end
       else
@@ -31,7 +33,7 @@ module Paperclip
           to_styles = {preview: "800x600>", thumb: "100x100>"}
         end
       end
-      return (allowed_content_types.include?(content_type) ? to_styles : fallback_styles)
+      return (options[:allowed_content_types].include?(content_type) ? to_styles : options[:fallback_styles])
     end
 
     ALLOWED_CONTENT_TYPES = ['application/pdf', 'image/png', 'image/x-png', 'image/gif', 'image/jpeg', 'image/pjpeg', 'image/jpg', 'image/tif', 'image/tiff', 'image/x-tiff']
